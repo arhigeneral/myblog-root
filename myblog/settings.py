5e4e05205eca71548a2e7fafd5dfba68792ae603
arhigeneral <48457530+arhigeneral@users.noreply.github.com>
2020-10-19 02:30:18 +0300

    New db add!

--- myblog/settings.py
+++ myblog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myblogdb',
+        'USER': 'postgres',
+        'PASSWORD': '1234567',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
