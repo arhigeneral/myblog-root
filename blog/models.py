af20e8d1ec4911b0c86a17f9d9811a93638ed228
arhigeneral <48457530+arhigeneral@users.noreply.github.com>
2020-10-19 14:23:20 +0300

    Posts add to admin panel

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    blog_title = models.CharField(max_length =  30)
+    blog_date = models.DateTimeField()
+    blog_image = models.ImageField(upload_to = 'blog_images/')
+    blog_text = models.TextField(max_length = 300)
