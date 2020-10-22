e2d6fb3127907d51716cde36a102bfc180534341
arhigeneral <48457530+arhigeneral@users.noreply.github.com>
2020-10-22 20:01:24 +0300

    Homepage upgrade + specific pages added!

--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -122,9 +123,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / 'myblog/static/')
 
+]
+
+STATIC_ROOT =  os.path.join(BASE_DIR / 'static')
 STATIC_URL = '/static/'
 
-MEDIE_ROOT =  BASE_DIR / 'MEDIE'
-
+MEDIE_ROOT =  os.path.join(BASE_DIR / 'media')
 MEDIE_URL = '/media/'
